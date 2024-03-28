b1c5a391b483797babf35b43a6e1afcf84769581
Elena Karpovich <elekarpovi@tut.by>
2024-03-28 12:56:12 +0300

    Added changes

--- myproject2/myapp/urls.py
+++ myproject2/myapp/urls.py
@@ -5,5 +5,5 @@ from .views import user_products, index
 urlpatterns = [
     path('user/<int:user_id>/<int:days>', user_products, name='user_products'),
     path('product/', views.product, name='product'),
-    path('', index, name='index'),
+    path('', views.index, name='index'),
 ]
